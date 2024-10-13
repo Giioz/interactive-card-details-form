fab27cdac8ec16db5f12133cdce79525aa55828d
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-13 20:15:19 +0400

    style: added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
 import { MobileBackgroundImage } from './components/background-images/Mobile-Background-Image'
+import { CardsSection } from './components/card-section/Cards-Section'
 
 function App() {
 
   return (
-    <MobileBackgroundImage />
-
+    <>
+      <MobileBackgroundImage />
+      <div className='px-[16px]'>
+        <CardsSection />
+      </div>
+    </>
   )
 }
 
