6bbd755e01c86f1686da95cb335743631e36eef4
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-14 16:02:03 +0400

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ export default {
       backgroundImage : {
         'custom-gradient': 'linear-gradient(to bottom right, rgb(255, 255, 255) 0%, rgb(210, 211, 217) 100%)',
       },
+      borderColor : {
+        'border-gradient': 'linear-gradient(to bottom right, rgb(99, 72, 254) 0%, rgbrgb(97, 5, 149) 100%)',
+      },
+      outlineColor : {
+        
+      }
     },
   },
   plugins: [],
