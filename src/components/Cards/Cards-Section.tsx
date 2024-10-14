ad82a38abd361ea4251b968e0ff9172352039c2e
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-14 16:02:45 +0400

    add : cards section component

---
+++ src/components/Cards/Cards-Section.tsx
@@ -0,0 +1,12 @@
+import { CardBack } from "./CardBack/Card-Back"
+import { CardFront } from "./CardFront/Card-Front"
+
+export const CardsSection = () => {
+  
+  return (
+    <div className="w-[343px] h-[251px] relative top-[32px] box-border mx-auto">
+      <CardFront />
+      <CardBack />
+    </div>
+  )
+}
