2c40688cbc2f05d05708251197e03796709ba92b
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-14 16:03:03 +0400

    add : card front

---
+++ src/components/Cards/CardFront/Card-Front.tsx
@@ -0,0 +1,23 @@
+export const CardFront = () => {
+  return (
+    <div className="bg-[url(./public/icons/card-mobile.svg)] w-[285px] h-[156.21px] absolute z-10 bottom-0 px-[19px] py-[17px] font-grotesk">
+      <div className="w-full flex items-center gap-[10px]">
+        <div className="w-[30px] h-[30px] rounded-full bg-white"></div>
+        <div className="w-[13px] h-[13px] rounded-full border-white border-[1px] inset-1"></div>
+      </div>
+      <div className="w-full mt-[37px] font-medium text-white">
+        <div>
+            <p className="text-[18px] tracking-[2.2px]">0000 0000 0000 0000</p>
+        </div>
+        <div className="text-[9px] tracking-[1.3px] flex justify-between mt-[17px] items-center">
+          <div>
+            <p>GIORGI MAMALADZE</p>
+          </div>
+          <div>
+            <p>07/27</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
