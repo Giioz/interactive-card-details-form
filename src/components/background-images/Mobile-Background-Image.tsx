8d9987efe32623c7cc88c5972f1fa8ff39965eee
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-17 21:51:53 +0400

    chore: updated src

--- src/components/background-images/Mobile-Background-Image.tsx
+++ src/components/background-images/Mobile-Background-Image.tsx
@@ -1,7 +1,7 @@
 export const MobileBackgroundImage = () => {
   return (
     <div className='w-full absolute top-0'>
-      <img src="../public/icons/mobile-background.svg" alt="backgroud" className='w-[100%] h-[240px] object-cover'/>
+      <img src="/icons/mobile-background.svg" alt="backgroud" className='w-[100%] h-[240px] object-cover'/>
     </div>
   )
 }
