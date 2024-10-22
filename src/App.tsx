a64065012061d61604a36a6c3bcf62e8db76c7af
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-22 21:17:47 +0400

    wip: redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import './App.css'
 import { MobileBackgroundImage } from './components/background-images/Mobile-Background-Image'
 import { CardsSection } from './components/Cards/Cards-Section'
 import { FormsSection } from './components/form/Forms-Section'
+import { store } from './redux/store/store'
+import { Provider } from 'react-redux'
 
 function App() {
 
   return (
     <>
       <MobileBackgroundImage />
-      <div className='px-[16px]'>
-        <CardsSection />
-      </div>
-      <FormsSection />
+      <Provider store={store}>
+          <div className='px-[16px]'>
+            <CardsSection />
+          </div>
+          <FormsSection />
+      </Provider>
     </>
   )
 }
