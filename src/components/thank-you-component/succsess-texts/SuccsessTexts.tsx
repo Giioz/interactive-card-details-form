3c957977d5872adf4ab36069b9b6b642b53cdaa3
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-24 22:43:20 +0400

    created section

---
+++ src/components/thank-you-component/succsess-texts/SuccsessTexts.tsx
@@ -0,0 +1,8 @@
+export const SuccsessTexts = () => {
+  return (
+    <div className="font-grotesk text-center mt-[35px] mb-[48px]">
+        <h2 className="text-[28px] text-[#21092F]">THANK YOU!</h2>
+        <p className="text-[18px] text-[#8F8694] mt-[16px]">We've added your card details</p>
+    </div>
+  )
+}
