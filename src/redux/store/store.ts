b36e7f226baf2010f289ff14d370734a4bfda4ca
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-24 22:43:54 +0400

    feat: registered isSubmited reducer

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cardSlice from "../features/card/cardSlice";
+import isSubmited from "../features/isSubmited/isSubmited";
 
 
 export const store = configureStore({
     reducer: {
+        isSubmited: isSubmited,
         card: cardSlice,
-    }
+
+    },
+
 })
 
 
