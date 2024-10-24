ae5c45e7f971fd3999b27e62f0212059731fe278
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-24 22:43:01 +0400

    deleted

--- src/components/form/Button/Button.tsx
+++
@@ -1,5 +0,0 @@
-export const Button = () => {
-  return (
-    <button type="submit" className="w-full mt-[8px] bg-[#21092F] py-[15px] px-[130px] text-white rounded-[8px] text-[18px] font-grotesk">Confirm</button>
-  )
-}
