5e400d81034c854301cd94119d11113baa5281fb
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-24 22:42:29 +0400

    added: button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,5 @@
+export const Button = ({ content, onclick }:any) => {
+  return (
+    <button onClick={onclick} type="submit" className="w-full mt-[8px] bg-[#21092F] py-[15px] px-[130px] text-white rounded-[8px] text-[18px] font-grotesk">{content}</button>
+  )
+}
