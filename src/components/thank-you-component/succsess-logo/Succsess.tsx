3acb7dbc806d5887dcae7eb23d9b9b5a0805ee32
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-24 22:43:14 +0400

    created section

---
+++ src/components/thank-you-component/succsess-logo/Succsess.tsx
@@ -0,0 +1,7 @@
+export const Succsess = () => {
+  return (
+    <div className="w-[80px] h-[80px] rounded-full bg-gradient-to-r from-[#6300fe] to-[#610596] flex justify-center items-center">
+        <img src="./icons/succsess.svg" alt="" className="w-[24px] h-[16px]"/>
+    </div>
+  )
+}
