58876a5d881ad89f78e8b04a848580db83cd166e
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-24 22:42:00 +0400

    added: succsess submit component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
+import { useState } from 'react'
 import './App.css'
 import { MobileBackgroundImage } from './components/background-images/Mobile-Background-Image'
 import { CardsSection } from './components/Cards/Cards-Section'
 import { FormsSection } from './components/form/Forms-Section'
-import { store } from './redux/store/store'
-import { Provider } from 'react-redux'
+import { ThankYouSection } from './components/thank-you-component/ThankYouSection'
+import { RootState, store } from './redux/store/store'
+import { useDispatch, useSelector } from 'react-redux'
+import { setIsSubmited } from './redux/features/isSubmited/isSubmited'
 
 function App() {
 
+  // default value is false !!!
+  const issubmited = useSelector((state:RootState) => state.isSubmited.isSubmited)
+  const dispatch = useDispatch()
+  console.log(issubmited);
+  
+
   return (
     <>
-      <Provider store={store}>
-          <MobileBackgroundImage />
-          <div className='px-[16px]'>
-            <CardsSection />
-          </div>
-          <FormsSection />
-      </Provider>
-
+      <MobileBackgroundImage />
+      <div className='px-[16px]' onClick={() => dispatch(setIsSubmited())}>
+        <CardsSection />
+      </div>
+      {!issubmited && (
+        <FormsSection />
+      )}
+      {issubmited && (
+        <ThankYouSection />
+      )}
     </>
   )
 }
