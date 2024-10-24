330fb6cb37811a33e489cfd7b5eb1be571c93fb0
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-24 20:41:44 +0400

    feat: state managment

--- src/components/Cards/CardBack/Card-Back.tsx
+++ src/components/Cards/CardBack/Card-Back.tsx
@@ -1,10 +1,15 @@
+import { useSelector } from "react-redux"
+import { RootState } from "../../../redux/store/store"
+
 export const CardBack = () => {
+  const card = useSelector((state: RootState) => state.card.card)
+  
   return (
     <div className="min-w-[280px] h-[157px] rounded-[6px] bg-custom-gradient absolute top-0 right-0 font-grotesk">
         <div className="w-full h-[34.6px] bg-[#2F2F2F] mt-[14.74px]">
         </div>
         <div className="w-[230px] h-[29.74px] bg-[#ADB5BE] rounded-[4px] mt-[15.39px] mx-auto flex justify-end items-center pr-[10px]">
-          <p className="text-[9px] text-white">000</p>
+          <p className="text-[9px] text-white">{card?.cvc}</p>
         </div>
         <div className="w-[100px] mx-auto mt-[15px]">
           <img src="./icons/card-background-title.svg" alt="bg" />
--- src/components/Cards/CardFront/Card-Front.tsx
+++ src/components/Cards/CardFront/Card-Front.tsx
@@ -1,26 +1,39 @@
 import { useSelector } from "react-redux"
 import { RootState } from "../../../redux/store/store"
+import { useEffect } from "react"
 
-export const CardFront = () => {1
+export const CardFront = () => {
   const card = useSelector((state: RootState) => state.card.card)
+
+  useEffect(() => {
+    formatedCardNumber = formatText(card!.cardNumber)
+    console.log(card?.cardNumber, formatedCardNumber);
+    
+  }, [card])
+
+  const formatText = ((cardNumb:string):string => {
+    return cardNumb.replace(/(.{4})/g, '$1 ')
+  })
+
+  let formatedCardNumber = formatText(card!.cardNumber)
   
   return (
-    <div className="bg-[url(./icons/card-mobile.svg)] w-[285px] h-[156.21px] absolute z-10 bottom-0 px-[19px] py-[17px] font-grotesk">
+    <div className="bg-[url(./icons/card-mobile.svg)] w-[285px] h-[156.21px] absolute z-10 bottom-0 px-[19px] py-[14px] font-grotesk">
       <div className="w-full flex items-center gap-[10px]">
         <div className="w-[30px] h-[30px] rounded-full bg-white"></div>
         <div className="w-[13px] h-[13px] rounded-full border-white border-[1px] inset-1"></div>
       </div>
       <div className="w-full mt-[37px] font-medium text-white">
         <div>
-            <p onClick={() => console.log(card)}className="text-[18px] tracking-[2.2px]">{card?.cardNumber}</p>
+            <p className="text-[18px] tracking-[2.2px]">{formatedCardNumber!}</p>
 
         </div>
-        <div className="text-[9px] tracking-[1.3px] flex justify-between mt-[17px] items-center">
+        <div className="text-[9px] tracking-[1.3px] flex justify-between mt-[17px] mb-[18px] items-center">
           <div>
-            <p>GIORGI MAMALADZE</p>
+            <p className="text-[14px]">{card?.cardHolder}</p>
           </div>
           <div>
-            <p>07/27</p>
+            <p>{card?.cardDate.mm}/{card?.cardDate.yy}</p>
           </div>
         </div>
       </div>
