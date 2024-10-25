00693d5f3f42a65c784bfd749f8f2ffa1ec16462
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-25 19:52:54 +0400

    added desktop bg

---
+++ src/components/background-images/DesktopBackgroundImage.tsx
@@ -0,0 +1,8 @@
+export const DesktopBackgroundImage = () => {
+    return (
+      <div className='w-[483px] absolute top-0'>
+        <img src="/icons/background.svg" alt="backgroud" className='w-[100%] h-full object-cover'/>
+      </div>
+    )
+  }
+  
